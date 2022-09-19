d3a8fbb20620a662f00f41ff2d4200612046f38d
Jahye Hwang <meltinp@gmail.com>
2022-09-19 22:04:04 +0900

    220919 difference between useState useEffect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import Button from "./Button";
 import PropTypes from "prop-types";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => {
+    setValue((prev) => prev + 1);
+  };
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
+  console.log("render");
+  useEffect(() => {
+    console.log("Am I calling API....?");
+  }, []);
+  useEffect(() => {
+    if (keyword !== 0 && keyword.length > 5) {
+      console.log(`search for ${keyword}`);
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I am running when counter changes");
+  }, [counter]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className={styles.title}>Hello World!</p>
-        Learn React
-        <Button text={"Push"} />
-      </header>
+    <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="search bar" />
+      <h1>Counter: {counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
