019d1aa2bbc7870bd66282db1881964fbbe74032
Jahye Hwang <meltinp@gmail.com>
2022-09-27 23:17:56 +0900

    220927 fetch api

--- src/App.js
+++ src/App.js
@@ -1,48 +1,89 @@
 import Button from "./Button";
-import PropTypes from "prop-types";
+import PropTypes, { symbol } from "prop-types";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
+import { toContainElement } from "@testing-library/jest-dom/dist/matchers";
 
-function Hello() {
-  useEffect(() => {
-    console.log("created");
-    return () => console.log("destroyed"); // a cleanup function
-  }, []);
-  return <h1>Hello</h1>;
-}
 function App() {
-  const [todo, setTodo] = useState("");
-  const [todos, setTodos] = useState([]);
-  const [del, removeTodos] = useState([]);
-  const onChange = (event) => setTodo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (todo === "") {
-      return;
-    }
-    setTodo("");
-    setTodos((currentArray) => [todo, ...currentArray]);
+  const [loading, completeLoading] = useState(false);
+  const [coin, setCoin] = useState([]);
+  const [option, setOption] = useState([]);
+  //option = option value (id)
+  const onSelect = (event) => {
+    setOption(event.target.value);
   };
-  const remove = (event) => {
-    setTodos([]);
-  };
-  console.log(todos);
+  // coin.find((coin) => coin.id === option)
+  // find selected object
+  const selectedCoin = coin.find((coin) => coin.id === option);
+  const defaultCoin = coin.find((coin) => coin.id === "btc-bitcoin");
+  console.log(typeof defaultCoin);
+
+  // console.log(selectedCoin);
+
+  const API_KEY = "https://api.coinpaprika.com/v1/tickers";
+  useEffect(() => {
+    fetch(API_KEY)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setCoin(data);
+        completeLoading(true);
+        console.log(data);
+      });
+  }, []);
+  useEffect(() => {
+    // for (const [key, value] of Object.entries(selectedCoin)) {
+    //   console.log(`${key}: ${value}`);
+    // }
+    console.log(typeof selectedCoin);
+  }, [option]);
+
   return (
     <div>
-      <h1>To Do List({todos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={todo} type="text" placeholder="write your todo" />
-        <button>Enter</button>
-      </form>
-      <hr />
-      <ul>
-        {todos.map((item, index) => (
-          <li key={index}>
-            <input type="checkbox" /> {item}
-          </li>
-        ))}
-      </ul>
-      <button onClick={remove}>clear</button>
+      <h1>Coin Tracker ({coin.length})</h1>
+      {loading ? null : <strong>Loading Now..</strong>}
+      <div>
+        <h3>Coin Converter</h3>
+        <select onChange={onSelect} defaultValue={defaultCoin} value={coin.id}>
+          {coin.map((coin) => (
+            <option value={coin.id} key={coin.id}>
+              {coin.name}({coin.symbol})
+            </option>
+          ))}
+        </select>
+
+        <div>
+          coin to $
+          <input type="text" readOnly value={option} />
+        </div>
+        <div>
+          coin to btc
+          <input type="text" readOnly value={selectedCoin} />
+        </div>
+        <div>
+          1$ to coin
+          <input type="text" readOnly value="hi" />
+          {JSON.stringify(selectedCoin)}
+        </div>
+      </div>
+      <div>
+        <ul>
+          {coin.map((coin) => (
+            <li className={styles.this} key={coin.id}>
+              {coin.name}({coin.symbol}) : {Intl.NumberFormat("en-Latn-US", { style: "currency", currency: "USD" }).format(coin.quotes.USD.price)}
+            </li>
+          ))}
+        </ul>
+        <input type="text" placeholder="put dollars" />
+        <select>
+          <option></option>
+        </select>
+        <button>convert</button>
+      </div>
+      <div>
+        <ul></ul>
+      </div>
     </div>
   );
 }
